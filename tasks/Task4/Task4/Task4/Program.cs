39a93b703d59480156a1b3535e0c7dde985d8196
Dominik Rychly <ic17b052@technikum-wien.at>
2018-04-01 13:49:30 +0200

    add project Task4

---
+++ tasks/Task4/Task4/Task4/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace Task4
+{
+
+    interface Car //Task 3.1
+    {
+        void Print();
+    }
+    public class Mercedes : Car //Task 3.2
+    {
+        public string Make;
+        public string Model;
+        public decimal Price;
+
+        public Mercedes(string make, string model, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(make)) throw new ArgumentException("Model must not be empty.", nameof(make));
+            if (string.IsNullOrWhiteSpace(model)) throw new ArgumentException("Model must not be empty.", nameof(model));
+            if (price <= 0) throw new ArgumentException("Wrong Price", nameof(price));
+            Make = make;
+            Model = model;
+            newPrice(price);
+        }
+
+        public string GetMake => Make;
+        public string GetModel => Model;
+        public decimal GetPrice => Price;
+        public void newPrice(decimal price)
+        {
+            if (price < 0) throw new ArgumentException("Price must not be negative.", nameof(price));
+            Price = price;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Dies ist ein:  { GetMake } - {GetModel} um {GetPrice} Euro");
+        }
+    }
+
+    class Audi : Car //Task 3.3
+    {
+        public string Make;
+        public string Model;
+        public string CarBodyDesign;
+
+        public Audi(string make, string model, string carbodydesign)
+        {
+            if (string.IsNullOrWhiteSpace(make)) throw new ArgumentException("Model must not be empty.", nameof(make));
+            if (string.IsNullOrWhiteSpace(model)) throw new ArgumentException("Model must not be empty.", nameof(model));
+            if (string.IsNullOrWhiteSpace(carbodydesign)) throw new ArgumentException("Model must not be empty.", nameof(carbodydesign));
+            Make = make;
+            Model = model;
+            CarBodyDesign = carbodydesign;
+
+        }
+
+        public string GetMake => Make;
+        public string GetModel => Model;
+        public string GetCarBodyDesign => CarBodyDesign;
+
+        public void Print()
+        {
+            Console.WriteLine($"Dies ist ein:  { GetMake } - {GetModel} - {GetCarBodyDesign}");
+        }
+    }
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Mercedes a = new Mercedes("Mercedes", "CLA", 40000);
+                a.Print();
+                a.newPrice(15555);
+                Mercedes b = new Mercedes("Mercedes", "Vito", 54587);
+                Audi c = new Audi("Audi", "A4", "Shooting Brake");
+                Audi d = new Audi("Audi", "R8", "Roadster");
+                Car[] CarArray = { a, b, new Mercedes("Mercedes", "A180", 16734), c, d, new Audi("TestAudi", "TestModel", "TestCarBodyDesign") }; //Task 3.4.1
+
+                foreach (Car Car in CarArray) //Task 3.4.2
+                {
+                    Car.Print();
+                }
+                
+                var jsonsettings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto }; //Task 4.3
+                var json = JsonConvert.SerializeObject(CarArray, jsonsettings);
+                Console.WriteLine(json);
+                                
+                var itemsfromjson = JsonConvert.DeserializeObject<Car[]>(json, jsonsettings); //Task 4.3
+                foreach (var Actuator in itemsfromjson)
+                {
+                    Actuator.Print();
+                }
+                
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Error({ error.Message})");
+            }
+        }
+
+    }
+
+}
---
+++ tasks/Task4/Task4/Task4/Tests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using NUnit.Framework;
+using Task4;
+
+//Task4.2
+
+namespace Task4
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+         public void CanCreateMercedes()
+        {
+              var x  = new Mercedes("Mercedes", "CLA", 40000);
+              Assert.IsTrue(x.Make == "Mercedes");
+              Assert.IsTrue(x.Model == "CLA");
+              Assert.IsTrue(x.Price == 40000);
+         }
+
+        [Test]
+        public void CanCreateAudi()
+        {
+            var x = new Audi("Audi", "R8", "Roadster") ;
+            Assert.IsTrue(x.Make == "Audi");
+            Assert.IsTrue(x.Model == "R8");
+            Assert.IsTrue(x.CarBodyDesign == "Roadster");
+        }
+
+        [Test]
+        public void CannotCreateNegativePrice()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Mercedes("Mercedes", "CLA", -40000);
+            });
+        }
+        [Test]
+        public void CannotCreateWithoutMake1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Audi(null, "CLA", "Roadster");
+            });
+        }
+        [Test]
+        public void CannotCreateWithoutMake2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Audi("", "CLA", "Roadster");
+            });
+        }
+        [Test]
+        public void CanUpdateMercedesPrice()
+        {
+            var x = new Mercedes("Mercedes", "CLA", 40000);
+            Assert.IsTrue(x.Price == 40000);
+            x.newPrice(70000);
+            Assert.IsTrue(x.Price == 70000);
+        }
+        [Test]
+        public void CannotUpdateNegativeMercedesPrice()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Mercedes("Mercedes", "CLA", 40000);
+                x.newPrice(-70000);
+            });
+        }
+        [Test]
+        public void CannotCreateWithoutModel1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Audi("Audi", null, "Roadster");
+            });
+        }
+        [Test]
+        public void CannotCreateWithoutModel2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Audi("Audi","", "Roadster");
+            });
+        }
+    }
+}
